d6356ebfc41a77424ee468ba88074866e701a2d3
Sergei <gannochenko.sv@gmail.com>
2019-07-17 22:22:08 +0200

    get city

--- app.api/src/api/cities.js
+++ app.api/src/api/cities.js
@@ -5,9 +5,23 @@ export default app => {
     app.get(
         '/cities',
         wrapError(async (req, res) => {
-            res.status(200)
+            return res
+                .status(200)
                 .header('Content-Type', 'application/json')
                 .send(JSON.stringify(cities));
+        }),
+    );
+    app.get(
+        '/cities/:id',
+        wrapError(async (req, res) => {
+            const id = parseInt(req.params.id, 10);
+            if (Number.isNaN(id)) {
+                return res.status(400).send();
+            }
+
+            res.status(200)
+                .header('Content-Type', 'application/json')
+                .send(JSON.stringify(cities.find(city => city.id === id)));
         }),
     );
 };
